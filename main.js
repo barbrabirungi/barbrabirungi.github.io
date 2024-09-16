883848aab11aa9572a9629ba615921e050a444bc
Barbra Birungi <86525834+barbrabirungi@users.noreply.github.com>
2024-09-16 10:20:20 +0300

    Update16.09.24

--- main.js
+++ main.js
@@ -2,25 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('.brief-summary').forEach(summary => {
         summary.addEventListener('click', function() {
             const detail = this.nextElementSibling;
-            detail.style.display = detail.style.display === 'flex' ? 'none' : 'flex';
+            const isOpen = detail.classList.contains('open');
+            detail.classList.toggle('open', !isOpen);
         });
-    });
-
-    const navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const section = document.querySelector(this.getAttribute('href') + ' .detailed-summary');
-            section.style.display = section.style.display === 'flex' ? 'none' : 'flex';
-        });
-    });
-
-    const backToTopButton = document.getElementById('back-top');
-    window.onscroll = function() {
-        backToTopButton.style.display = window.pageYOffset > 20 ? 'block' : 'none';
-    };
-
-    backToTopButton.addEventListener('click', function() {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 });
