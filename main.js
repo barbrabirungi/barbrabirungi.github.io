f148b961ff5c860ec5cf17550238156a571fde84
Barbra Birungi <86525834+barbrabirungi@users.noreply.github.com>
2024-09-16 10:29:43 +0300

    16.09.24-01

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
         summary.addEventListener('click', function() {
             const detail = this.nextElementSibling;
             const isOpen = detail.classList.contains('open');
+            if (isOpen) {
+                detail.style.maxHeight = '0';  // Collapse section
+                detail.style.opacity = '0';    // Fade out section
+            } else {
+                detail.style.display = 'flex'; // Make flex to enable inner content alignment
+                detail.style.opacity = '1';    // Fade in section
+                detail.style.maxHeight = '1000px'; // Max height for animation
+            }
             detail.classList.toggle('open', !isOpen);
         });
     });
